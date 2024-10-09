d27a2ed6737dc0ae142cb5fb402ef2189cae9f7a
nbayashi <naoya_nstyle@hotmail.co.jp>
2024-10-10 00:30:57 +0900

    update

--- shred_layer_dialog.py
+++ shred_layer_dialog.py
@@ -24,6 +24,7 @@
 
 import os
 import glob
+import random
 import qgis
 
 from qgis.PyQt import uic
@@ -55,97 +56,134 @@ class shredlayerDialog(QtWidgets.QDialog, FORM_CLASS):
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
 
-        #setting slider
-        self.horizontalSlider.setMinimum(1)
+        # setting slider
+        self.horizontalSlider.setMinimum(2)
         self.horizontalSlider.setMaximum(10)
         self.horizontalSlider.setSingleStep(1)
         self.horizontalSlider.setPageStep(1)
 
-        self.radioButton.setChecked(True)
+        self.radioButton_vertical.setChecked(True)
+        self.layer_ComboBox.layerChanged.connect(
+            self.checkable)
 
-        # 実行ボタン
+        # button setting
         self.button_box.accepted.connect(self.run_shred)
         self.button_box.rejected.connect(self.dlg_close)
-
 
     def dlg_close(self):
         self.close()
 
-
+    def checkable(self):
+        if self.layer_ComboBox.currentLayer().type() == 0:
+            self.groupBox_option.setEnabled(True)
+            self.groupBox_option.setCheckable(True)
+            self.groupBox_option.setChecked(False)
+        else:
+            self.groupBox_option.setEnabled(False)
 
     def run_shred(self):
 
-        layer = self.layer_ComboBox.currentLayer()
-        shrednum = self.horizontalSlider.value()*10
+        input_layer = self.layer_ComboBox.currentLayer()
+        shrednum = self.horizontalSlider.value()
 
-
-
-
-
-        (Dir,filename) = os.path.split(layer.dataProvider().dataSourceUri())
+        (Dir, filename) = os.path.split(
+            input_layer.dataProvider().dataSourceUri())
         os.chdir(Dir)
 
-
-
-        #getExtent
-        box = layer.extent()
+        # getExtent
+        box = input_layer.extent()
         xmin = box.xMinimum()
         xmax = box.xMaximum()
         ymin = box.yMinimum()
         ymax = box.yMaximum()
 
-        exta = str(xmin) + ',' + str(xmax) + ',' + str(ymin) + ',' +str(ymax)
+        exta = str(xmin) + ',' + str(xmax) + ',' + str(ymin) + ',' + str(ymax)
 
-
-        #VectorGrid
-        if self.radioButton.isChecked() == True:
-            shredGrid = processing.runAndLoadResults("qgis:creategrid",{'TYPE':2,'EXTENT':exta,'HSPACING':box.width()/shrednum,'VSPACING':box.height(),'HOVERLAY':0,'VOVERLAY':0,'CRS':layer.crs().authid(),'OUTPUT':'memory:'})
-        elif self.radioButton_2.isChecked() == True:
-            shredGrid = processing.runAndLoadResults("qgis:creategrid",{'TYPE':2,'EXTENT':exta,'HSPACING':box.width(),'VSPACING':box.height()/shrednum,'HOVERLAY':0,'VOVERLAY':0,'CRS':layer.crs().authid(),'OUTPUT':'memory:'})
-
-
-        #shredlayer =  QgsProject.instance().mapLayersByName("グリッドベクタの出力")[0]
-        temp_layers = QgsProject.instance().mapLayers()
-       
-        shredlayer =  temp_layers[list(temp_layers.keys())[0]]
-
-
-
-
-        if layer.type() == 0:
-            #Vector
-            for i in range(1,shrednum+1,1):
-                shredlayer.selectByExpression('"id" =' + str(i),QgsVectorLayer.SetSelection)
-                
-                
-                processing.runAndLoadResults("native:clip", {"INPUT": layer, "OVERLAY":QgsProcessingFeatureSourceDefinition(shredGrid['OUTPUT'],True) , "OUTPUT": Dir + '/shred_'+layer.name() + "_" +  str(i) + '.shp'})
+        # VectorGrid
+        if self.radioButton_vertical.isChecked() == True:
+            shredgrid = processing.run("qgis:creategrid", {'TYPE': 2, 'EXTENT': exta, 'HSPACING': box.width(
+            )/(shrednum*10), 'VSPACING': box.height(), 'HOVERLAY': 0, 'VOVERLAY': 0, 'CRS': input_layer.crs().authid(), 'OUTPUT': 'memory:'})['OUTPUT']
+        elif self.radioButton_horizonal.isChecked() == True:
+            shredgrid = processing.run("qgis:creategrid", {'TYPE': 2, 'EXTENT': exta, 'HSPACING': box.width(
+            ), 'VSPACING': box.height()/(shrednum*10), 'HOVERLAY': 0, 'VOVERLAY': 0, 'CRS': input_layer.crs().authid(), 'OUTPUT': 'memory:'})['OUTPUT']
         else:
-            #Raster
-            for i in range(1,shrednum+1,1):
-                shredlayer.selectByExpression('"id" =' + str(i),QgsVectorLayer.SetSelection)
-                
-                
-                rasClip =processing.runAndLoadResults("gdal:cliprasterbymasklayer", {"INPUT": layer, "MASK":QgsProcessingFeatureSourceDefinition(shredGrid['OUTPUT'],True) ,"NODATA":None,"ALPHA_BAND":False, "CROP_TO_CUTLINE":True,"KEEP_RESOLUTION":True, "OPTIONS":'',"DATA_TYPE":0,"OUTPUT":Dir + '/shred_'+ layer.name() + "_" + str(i) + '.tif'})
-       
-        #remove layer
-        QgsProject.instance().removeMapLayer(layer)
-        QgsProject.instance().removeMapLayer(shredlayer)
+            shredgrid = processing.run("qgis:creategrid", {'TYPE': 2, 'EXTENT': exta, 'HSPACING': box.width(
+            )/shrednum, 'VSPACING': box.height()/shrednum, 'HOVERLAY': 0, 'VOVERLAY': 0, 'CRS': input_layer.crs().authid(), 'OUTPUT': 'memory:'})['OUTPUT']
 
-        layer = None
+        # run clip
+        extent_list = []
+        clipped_list = []
+        if input_layer.type() == 0:
+            # shredgrid を順に指定
+            for feat in shredgrid.getFeatures():
+                featId = feat.id()
+                selection = shredgrid.selectByExpression(
+                    '$id=' + str(featId))
+                selectedlyr = shredgrid.materialize(
+                    QgsFeatureRequest().setFilterFids(shredgrid.selectedFeatureIds()))
+                # get bbox
+                extent_list.append(selectedlyr.extent())
+                clipped_output = processing.run("native:clip", {
+                                                "INPUT": input_layer, "OVERLAY": selectedlyr, "OUTPUT": Dir + "/shred_"+input_layer.name() + "_" + str(featId) + '.shp'})['OUTPUT']
+                clipped = QgsVectorLayer(
+                    clipped_output, input_layer.name() + "_" + str(featId), 'ogr')
+                QgsProject.instance().addMapLayer(clipped)
+                clipped_list.append(clipped)
+        else:
+            for feat in shredgrid.getFeatures():
+                featId = feat.id()
+                selection = shredgrid.selectByExpression(
+                    '$id=' + str(featId))
+                selectedlyr = shredgrid.materialize(
+                    QgsFeatureRequest().setFilterFids(shredgrid.selectedFeatureIds()))
+                # get bbox
+                extent_list.append(selectedlyr.extent())
+
+                rasClip = processing.run("gdal:cliprasterbymasklayer", {'INPUT': input_layer, 'MASK': selectedlyr, 'SOURCE_CRS': None, 'TARGET_CRS': None, 'NODATA': None, 'ALPHA_BAND': False, 'CROP_TO_CUTLINE': True,
+                                                                        'KEEP_RESOLUTION': False, 'SET_RESOLUTION': False, 'X_RESOLUTION': None, 'Y_RESOLUTION': None, 'MULTITHREADING': False, 'OPTIONS': '', 'DATA_TYPE': 0, 'EXTRA': '', 'OUTPUT':  Dir + "/shred_" + input_layer.name() + "_" + str(featId) + '.tif'})['OUTPUT']
+                clipped = QgsRasterLayer(
+                    rasClip, input_layer.name() + "_" + str(featId), input_layer.dataProvider().name())
+                QgsProject.instance().addMapLayer(clipped)
+
+        # shuffle
+        if self.groupBox_option.isEnabled() == True and self.groupBox_option.isChecked() == True and (self.radioButton_shuffle.isChecked() == True or self.radioButton__collage.isChecked() == True):
+            random_list = random.sample(extent_list, len(extent_list))
+            diff_xlist = [rl.xMaximum() - l.xMaximum()
+                          for (rl, l) in zip(random_list, extent_list)]
+            diff_ylist = [rl.yMaximum() - l.yMaximum()
+                          for (rl, l) in zip(random_list, extent_list)]
+            # collage
+            if self.radioButton__collage.isChecked() == True:
+                dx = extent_list[0].xMaximum() - extent_list[0].xMinimum()
+                dy = extent_list[0].yMaximum() - extent_list[0].yMinimum()
+                for i in range(len(extent_list)):
+                    diff_xlist[i] = diff_xlist[i] + \
+                        dx * random.uniform(-1.0, 1.0)
+                    diff_ylist[i] = diff_ylist[i] + \
+                        dy * random.uniform(-1.0, 1.0)
+
+            # change geometry
+            for i in range(len(clipped_list)):
+                for clipped_feature in clipped_list[i].getFeatures():
+                    geom = clipped_feature.geometry()
+                    geom.translate(diff_xlist[i], diff_ylist[i])
+                    clipped_list[i].dataProvider().changeGeometryValues(
+                        {clipped_feature.id(): geom})
+                    iface.mapCanvas().refreshAllLayers()
+                    iface.mapCanvas().refresh()
+                    clipped_list[i].triggerRepaint()
+
+        # remove layer
+        QgsProject.instance().removeMapLayer(input_layer)
+
+        # refresh
+        input_layer = None
         iface.mapCanvas().refresh()
-
-
-        #shp一式選択
+        iface.mapCanvas().refreshAllLayers()
+        # shp一式選択
         files = glob.glob(filename.split('shp')[0]+'*')
 
-
-        #Delete
+        # Delete
         for i in files:
             os.remove(i)
-
         self.close()
-
-
-        
-
-
